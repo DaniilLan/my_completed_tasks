9425f8538918955e9f9870d8f56f918e87f21b1b
Daniil <95534346+DaniilLan@users.noreply.github.com>
2023-04-07 23:12:57 +0400

    Update SF_13.8.19.py

--- SF_13.8.19.py
+++ SF_13.8.19.py
@@ -2,8 +2,6 @@ quantity_ticket = int(input("Введите количество билетов:
 
 all_price = 0
 
-itog = f"Стоимость всех билетов = {all_price} руб"
-
 for i in range(quantity_ticket):
   age = int(input(f"Введите возраст для {i+1} билета: "))
   if 18 <= age < 25:
@@ -13,6 +11,7 @@ for i in range(quantity_ticket):
 
 if quantity_ticket > 3:
   all_price = all_price - all_price * 0.1
-  itog += ", c учётом скидки в 10%"
-  
-print(itog)
+  print(f"Стоимость всех билетов = {int(all_price)} руб, c учётом скидки в 10%")
+else:
+  print(f"Стоимость всех билетов = {int(all_price)} руб")
+
